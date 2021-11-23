be7ece76695714a61701083771bcacfbd03cabef
Rafael Mayer <rafa.m.mayer@hotmail.com>
2021-11-23 18:25:10 -0300

    feature: implement fontawesome

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import SobreMim from "./SobreMim";
 
 export default function App(props) {
   return (
-    <div className="text-quaternary">
+    <section>
+      <div className="text-quaternary">
       <h1>{props.name}</h1>
       <h2>{props.objective}</h2>
-      <SobreMim email="rafa.m.mayer@hotmail.com" />
     </div>
+    <SobreMim email="rafa.m.mayer@hotmail.com" />
+    </section>
+    
   )
 }
