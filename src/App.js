81cedf1b925a8169c5e19a7fd4424658a7e323ca
Rafael Mayer <rafa.m.mayer@hotmail.com>
2021-11-24 20:27:47 -0300

    todo => implement dinamic render of habilities

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App(props) {
     <section className="text-secondary">
       <div className="text-center font-bold">
         <h1 className="text-4xl">{props.name}</h1>
-        <h2 className="my-3 text-1xl">{props.objective}</h2>
+        <h2 className="my-3 text-xl">{props.objective}</h2>
       </div>
       <Perfil email="rafa.m.mayer@hotmail.com" />
       <FormacaoEHabilidade />
